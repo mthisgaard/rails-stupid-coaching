aba04ccfc3e5d97a11e3449a4fdacf05dd6a6be0
Malene Thisgaard <malene.thisgaard@gmail.com>
2022-08-08 12:10:01 +0900

    add style

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -6,7 +6,7 @@ class QuestionsController < ApplicationController
   def answer
     @answer = if params[:question] == 'I am going to work'
       'Great'
-    elsif params[:question][-1] == '?'
+    elsif params[:question].include?('?')
       'Silly question, get dressed and go to work!.'
     else
       "I don't care, get dressed and go to work!"
